a222087f725617d0c71df1c768a5462f471ac2ae
Mukesh Otwani <mukeshotwani.50@gmail.com>
2026-07-19 08:40:22 +0530

    added group name and test name

---
+++ tests/regression/registration-test.spec.js
@@ -0,0 +1,15 @@
+import {test,expect} from "../../fixture/fixture.js"
+import data from "../../data/json/validcredentials.json"
+import dataset from "../../data/json/invalidcredentials.json"
+
+test.describe("New User Creation Test",()=>{
+
+    test("Verify user can create new users with registration Page", async ({page,loginPage,dashboardPage})=>
+    {
+        
+
+    
+    })
+
+
+})
