44d02b948344ba17494f6efe90434d7a9e98dff8
Mukesh Otwani <mukeshotwani.50@gmail.com>
2026-08-22 08:41:09 +0530

    Update login-test.spec.js

--- tests/smoke/login-test.spec.js
+++ tests/smoke/login-test.spec.js
@@ -14,7 +14,7 @@ test.describe("Login Test Scenarios",()=>{
 
         await dashboardPage.logoutFromApplication()
 
-        await expect(page).toHaveURL("/login")   
+        await expect(page).toHaveURL("/mukesh")   
         
 
     })
